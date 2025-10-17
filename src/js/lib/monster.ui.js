cb0173759372c523edd8fa9dc7769d7b8783cca2
Enrique Sanchez <enrique.sanchez@login.com>
2025-10-17 14:05:11 -0700

    Replace deprecated jQuery .bind()/.unbind() with .on()/.off() in monster.ui.js
    - Replaced .unbind().bind() with .off().on() in greaterDate validator
    
    jQuery 3.0+ deprecated .bind() and .unbind() in favor of .on() and .off()

--- src/js/lib/monster.ui.js
+++ src/js/lib/monster.ui.js
@@ -1348,12 +1348,12 @@ define(function(require) {
 
 			// Adding advanced custom rules
 			$.validator.addMethod('greaterDate', function(value, element, param) {
-				var target = _.isString(param) ? $(param) : param;
-				if (this.settings.onfocusout) {
-					target.unbind('.validate-greaterDate').bind('blur.validate-greaterDate', function() {
-						$(element).valid();
-					});
-				}
+			var target = _.isString(param) ? $(param) : param;
+			if (this.settings.onfocusout) {
+				target.off('.validate-greaterDate').on('blur.validate-greaterDate', function() {
+					$(element).valid();
+				});
+			}
 
 				return parseInt(monster.util.timeToSeconds(value)) > parseInt(monster.util.timeToSeconds(target.val()));
 			}, localization.customRules.greaterDate);
